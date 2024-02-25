1c43057189a6c57594bcc9a7f542ef91c6e9df89
e41c <ericgrigor@gmail.com>
2024-02-25 16:13:08 -0500

    added CRUD to the shopping list, allowing users to search all items added, to view the shopping list, and also edit items. Next plan to work on calucator, by importing the list from database, then adding calucator functions

--- lib/screens/shopping_list_screen.dart
+++ lib/screens/shopping_list_screen.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 
 class Item {
+  String id; // Added to hold Firestore document ID
   String name;
   double price;
   String group;
 
-  Item({required this.name, required this.price, required this.group});
+  Item({this.id = '', required this.name, required this.price, required this.group});
+
+  factory Item.fromFirestore(DocumentSnapshot doc) {
+    Map data = doc.data() as Map<String, dynamic>;
+    return Item(
+      id: doc.id,
+      name: data['name'] ?? '',
+      price: data['price']?.toDouble() ?? 0.0,
+      group: data['group'] ?? '',
+    );
+  }
 }
 
 class ShoppingListScreen extends StatefulWidget {
@@ -24,27 +35,96 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
 
   void _addItem() async {
     if (_formKey.currentState!.validate()) {
-      final item = Item(
-        name: _nameController.text,
-        price: double.parse(_priceController.text),
-        group: _selectedGroup ?? groups.first, // Default to first group if none selected
-      );
       await FirebaseFirestore.instance.collection('items').add({
-        'name': item.name,
-        'price': item.price,
-        'group': item.group,
+        'name': _nameController.text,
+        'price': double.parse(_priceController.text),
+        'group': _selectedGroup ?? groups.first,
       });
 
       // Clear the input fields and reset the dropdown after the item is added
       _nameController.clear();
       _priceController.clear();
-      setState(() {
-        _selectedGroup = null; // Reset selected group
-      });
+      setState(() => _selectedGroup = null);
 
-      // Optionally, show a confirmation message
+      // Show a confirmation message
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Item added successfully')));
     }
+  }
+
+  Stream<List<Item>> _fetchItems() {
+    return FirebaseFirestore.instance.collection('items').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Item.fromFirestore(doc)).toList());
+  }
+
+  void _deleteItem(String id) async {
+    await FirebaseFirestore.instance.collection('items').doc(id).delete();
+  }
+
+  Future<void> _showEditItemDialog(Item item) async {
+    _nameController.text = item.name;
+    _priceController.text = item.price.toString();
+    _selectedGroup = item.group;
+
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Edit Item'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(hintText: "Enter item name"),
+                ),
+                TextFormField(
+                  controller: _priceController,
+                  decoration: InputDecoration(hintText: "Enter price"),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                ),
+                DropdownButtonFormField<String>(
+                  value: _selectedGroup,
+                  hint: Text('Select a Group'),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedGroup = newValue;
+                    });
+                  },
+                  items: groups.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Update'),
+              onPressed: () async {
+                if (_nameController.text.isNotEmpty && _priceController.text.isNotEmpty && _selectedGroup != null) {
+                  await FirebaseFirestore.instance.collection('items').doc(item.id).update({
+                    'name': _nameController.text,
+                    'price': double.parse(_priceController.text),
+                    'group': _selectedGroup,
+                  });
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -58,69 +138,104 @@ class _ShoppingListScreenState extends State<ShoppingListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Item'),
+        title: const Text('Shopping List'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Item Name',
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter an item name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _priceController,
-                decoration: const InputDecoration(
-                  labelText: 'Price',
-                ),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the price';
-                  }
-                  if (double.tryParse(value) == null) {
-                    return 'Please enter a valid number';
-                  }
-                  return null;
-                },
-              ),
-              DropdownButtonFormField<String>(
-                value: _selectedGroup,
-                hint: Text('Select a Group'),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedGroup = newValue;
-                  });
-                },
-                validator: (value) => value == null ? 'Please select a group' : null,
-                items: groups.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: _addItem,
-                  child: const Text('Add Item'),
-                ),
-              ),
-            ],
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<List<Item>>(
+              stream: _fetchItems(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+                final items = snapshot.data!;
+                return ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    final item = items[index];
+                    return ListTile(
+                      title: Text('${item.name} - \$${item.price.toStringAsFixed(2)} (${item.group})'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () => _showEditItemDialog(item),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () => _deleteItem(item.id),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Item Name',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter an item name';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _priceController,
+                    decoration: const InputDecoration(
+                      labelText: 'Price',
+                    ),
+                    keyboardType: TextInputType.numberWithOptions(decimal: true),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the price';
+                      }
+                      if (double.tryParse(value) == null) {
+                        return 'Please enter a valid number';
+                      }
+                      return null;
+                    },
+                  ),
+                  DropdownButtonFormField<String>(
+                    value: _selectedGroup,
+                    hint: Text('Select a Group'),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedGroup = newValue;
+                      });
+                    },
+                    validator: (value) => value == null ? 'Please select a group' : null,
+                    items: groups.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: _addItem,
+                      child: const Text('Add Item'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
